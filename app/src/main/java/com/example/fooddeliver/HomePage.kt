cf748530ca1bcb39c68d655454396e2a30350e8a
droidrdee <deepak.yourdomain@gmail.com>
2025-02-24 18:38:12 +0530

    Did some changes

--- app/src/main/java/com/example/fooddeliver/HomePage.kt
+++ app/src/main/java/com/example/fooddeliver/HomePage.kt
@@ -5,7 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.fooddeliver.databinding.ActivityLocationPageBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomePage : AppCompatActivity() {
 
@@ -13,6 +16,10 @@ class HomePage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home_page)
+
+        val NavController = findNavController(R.id.fragmentContainerView3)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.setupWithNavController(NavController)
 
 
     }
